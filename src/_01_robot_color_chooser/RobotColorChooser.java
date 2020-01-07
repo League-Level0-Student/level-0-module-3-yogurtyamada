2f298be773507746a08de89d168c626927f23e8d
League Student <league@iMac-23.attlocal.net>
2020-01-06 19:57:02 -0800

    Update RobotColorChooser.java

--- src/_01_robot_color_chooser/RobotColorChooser.java
+++ src/_01_robot_color_chooser/RobotColorChooser.java
@@ -4,16 +4,22 @@
 package _01_robot_color_chooser;
 
 import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
 import java.awt.Color;
 
 public class RobotColorChooser {
 	public static void main(String[] args) {
 		//1. Create a new Robot
-		
+		Robot qwe=new Robot("batman");
 		//3. Ask the user what color they would like the robot to draw
-		
+		String input=JOptionPane.showInputDialog("what color");
 		//5. Use an if/else statement to set the pen color that the user requested
-
+if(input=="blue"){
+	System.out.println("blue");
+}
+}
         //6. If the user doesn’t enter anything, choose a random color
 
         //7. Put a loop around your code so that you keep asking the user for more colors & drawing them
@@ -24,4 +30,4 @@ public class RobotColorChooser {
 
 
 	}
-}
+
